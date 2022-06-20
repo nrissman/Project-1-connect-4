d9c65b2cebb6d821a34a9e995162b5afdff09953
nrissman <nrissman90@gmail.com>
2022-06-20 16:44:57 -0400

    updated HTML to divs instead of table, spots are clickable

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+//grab the s divs or the div "board"  and turn them into an array or loop over them some how
+//maybe turn the s divs into a ul 
+
+//
+
+//grabbing the "board" div
+const slots = document.getElementsByClassName('slot')
+//let slots = board.getElementsByTagName('div') 
+//possibly create  ul and append the "s"divs to it and loop through the ul 
+for (let i = 0; i < slots.length; i++) {
+    slots[i].onclick = () => {
+        console.log("hello world")
+    }
+}
+
+
+    //s1.addEventListener('click', function (event){
+    // console.log("hello world")
 
 
 
