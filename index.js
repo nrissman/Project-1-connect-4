da1ff80085d97d67055e3906edcda83b1e3608a5
nrissman <nrissman90@gmail.com>
2022-06-24 13:11:17 -0400

    all spots playable and win/tie functions work

--- index.js
+++ index.js
@@ -20,6 +20,7 @@
 //player one 
 //player two
 
+//selectors
 //make a container and grab it 
 const board = document.querySelector('.board')
 //this will alow us tho alternate whos turn it is
@@ -28,6 +29,7 @@ let playerColor = document.querySelector('.color')
 const result = document.querySelector('#result')
 let turns = 0
 let currentPlayer = 1
+let enabled = true
 let winningArrays = [ 
     [0, 1, 2, 3], [41, 40, 39, 38],[7, 8, 9, 10], 
     [34, 33, 32, 31], [14, 15, 16, 17], [27, 26, 25, 24], 
@@ -72,37 +74,38 @@ function loadGame () {
 }
 
         //making the board
-    const makeBoard = () => {
+const makeBoard = () => {
         //create 42 spots
-        for (let i = 0; i < 42; i++){
-            //create a div for every spot 
-            let div = document.createElement('div')
-            // giving the div a data set for later use
-            div.setAttribute('data-id', i)
-            div.setAttribute('data-avail', false)
-            //add the class of spot to newly created div
-            div.classList.add('spot')
-            //setting the bottom 7 spots class to 'available'
-            if (i>=35){
-                div.setAttribute('data-avail', true)
-                
-
-            }
-            console.log("the div created", div)
-            //add spots to the board
-            board.appendChild(div)
+    for (let i = 0; i < 42; i++){
+        //create a div for every spot 
+        let div = document.createElement('div')
+        // giving the div a data set for later use
+        div.setAttribute('data-id', i)
+        div.setAttribute('data-avail', false)
+        //add the class of spot to newly created div
+        div.classList.add('spot')
+        //setting the bottom 7 spots class to 'available'
+        if (i>=35){
+            div.setAttribute('data-avail', true)            
         }
-    }       
+        //console.log("the div created", div)
+        //add spots to the board
+        board.appendChild(div)
+    }
+}       
     //giving the spot a click function
 function clickSpot(e){
-    console.log('this is the spot: dataset', e.target.dataset)
+    //console.log('this is the spot: dataset', e.target.dataset)
     const available = e.target.dataset.avail === 'true' ? true : false 
-    console.log('available?', available)
+    let spotAboveId = (e.target.dataset.id - 7)
+    //console.log(spotAboveId)
+    //console.log(e.target.dataset.avail)
+   // console.log('available?', available)
     //needs to grab all the divs and make them selectable
     let spot = document.querySelectorAll('.board div') 
     let select = parseInt(this.dataset.id) 
     if (available) {
-        console.log('this spot is available')
+        //console.log('this spot is available')
         if(currentPlayer===1){  
             //changes player turn 
             document.getElementById('player').innerHTML = 'Yellows Turn'
@@ -111,28 +114,70 @@ function clickSpot(e){
             //switches player turn
             currentPlayer=2
             //makes spot unclickable
-            e.target.dataset.avail = 'false'   
-        }    else if(currentPlayer === 2){  
+            e.target.dataset.avail = 'false' 
+            console.log(e.target.dataset)
+            turns = turns +1
+        } else if(currentPlayer === 2){  
             document.getElementById("player").innerHTML = 'reds Turn'
             this.className="player-yellow" 
             currentPlayer=1 
-            e.target.dataset.avail = 'false'    
-        }       
-    }else{
-        alert('spot not available try somewhere else')
-        //console.log('spot not available try somewhere else')
-    }        
+            e.target.dataset.avail = 'false' 
+            turns = turns +1
+        } if (spotAboveId>=0){
+            const spotAbove = document.querySelector(`[data-id="${spotAboveId}"]`)            
+            spotAbove.dataset.avail = 'true'
+            
+        }
+        
+    } else alert('spot not available try somewhere else')
+    checkWin()    
+    //
+    //console.log('spot not available try somewhere else')
+        //console.log(turns)
         
 }
 
-    
-    
-    
 
+function checkWin() {
+    let spot = document.querySelectorAll('.board div') 
+    for (let y = 0; y < winningArrays.length; y++) {
+        const spots1 = spot[winningArrays[y][0]]
+        const spots2 = spot[winningArrays[y][1]]
+        const spots3 = spot[winningArrays[y][2]]
+        const spots4 = spot[winningArrays[y][3]]
+        
+            //check those squares to see if they all have the class of player-Red
+        if (
+            spots1.classList.contains('player-red') &&
+            spots2.classList.contains('player-red') &&
+            spots3.classList.contains('player-red') &&
+            spots4.classList.contains('player-red')
+            )
+        {
+            result.innerHTML = 'Player-red Wins!'
+        }
+            //check those squares to see if they all have the class of player-Yellow
+        if (
+            spots1.classList.contains('player-yellow') &&
+            spots2.classList.contains('player-yellow') &&
+            spots3.classList.contains('player-yellow') &&
+            spots4.classList.contains('player-yellow')
+            )
+        {
+            result.innerHTML = 'Player-yellow Wins!'
+        }
+        if (turns === 42) {
+            result.innerHTML = "It's a Tie. Click Reset Game to play again!"
+    }
+}
+}
+    
+    function endGame(){
+        let spot = document.querySelectorAll('.board div')
+        console.log('.board div')
+    }
 
-        // function checkWin(){
-
-        // }
+        
     
 
 
